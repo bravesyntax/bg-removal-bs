c24c132561967119c09004d0ef1b0ec439923fa1
Deepak kumar <93255664+Deepak-Angry-Coder@users.noreply.github.com>
2025-03-25 19:41:35 +0530

    server updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,31 @@
 import { Svix } from "svix";
 import userModel from "../models/userModel.js";
+// API controller function ro manage clerk user with database
+//http://localhost:4000/api/v1/user/webhooks
 
-// API controller function to manage Clerk webhooks
 const clerkWebHooks = async (req, res) => {
   try {
-    const wHook = new Svix(process.env.CLERK_WEBHOOK_SECRET);
-
-    // Verify the webhook
-    await wHook.verify(req.headers, req.body);
+    const wHook = await Svix(process.env.CLERK_WEBHOOK_SECRET);
+    await wHook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
 
     const { data, type } = req.body;
-
-    const payload = JSON.parse(req.body.toString());
-    console.log("Parsed Webhook Data:", payload);
 
     switch (type) {
       case "user.created": {
         const userData = {
           clerkId: data.id,
-          email: data.email_addresses?.[0]?.email_address || "",
-          firstName: data.first_name || "",
-          lastName: data.last_name || "",
-          photo: data.image_url || "",
+          email: data.email_addresses[0].email_address,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          photo: data.image_url,
         };
 
         await userModel.create(userData);
-        res.status(200).json({ success: true });
+        res.json();
 
         break;
       }
@@ -39,25 +39,24 @@ const clerkWebHooks = async (req, res) => {
         };
 
         await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
-        res.status(200).json({ success: true });
+        res.json({});
 
         break;
       }
 
       case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id });
-        res.status(200).json({ success: true });
+        res.json({});
         break;
       }
 
       default:
-        res.status(400).json({ success: false, message: "Unknown event type" });
         break;
     }
   } catch (error) {
-    console.error("Webhook Error:", error.message);
-    res.status(400).json({
-      success: false,
+    console.log(error.message);
+    res.json({
+      success: "false",
       message: error.message,
     });
   }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,10 +3,6 @@ import { clerkWebHooks } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/webhooks",
-  express.raw({ type: "application/json" }),
-  clerkWebHooks
-);
+userRouter.post("/webhooks", clerkWebHooks);
 
 export default userRouter;
